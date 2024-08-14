069a7c8299618943a2207207737a5ffb3ccce1a6
liyang <liyang@huocheetc.com>
2024-08-15 07:25:54 +0800

    Initial Commit

---
+++ weibo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weibo
+//
+//  Created by liyang on 2024/8/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
