41949bc3f0821edc9959ee897f4132e972288dd8
liyang <liyang@huocheetc.com>
2024-08-16 06:58:04 +0800

    验证访客视图解决方案

---
+++ weibo/Classes/View/Main/Visitor/VisitorTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  VisitorTableViewController.swift
+//  weibo
+//
+//  Created by liyang on 2024/8/16.
+//
+
+import UIKit
+
+class VisitorTableViewController: UITableViewController {
+
+  private var userLogin = false;
+  
+  override func loadView() {
+    super.loadView()
+//    view = UIView()
+//    view.backgroundColor = UIColor.orange
+  }
+
+}
