c03f227a1abb0e8f7d71bd789bc288956ff64f87
liyang <liyang@huocheetc.com>
2024-08-16 22:24:30 +0800

    访客视图布局

--- weibo/Classes/View/Main/Visitor/VisitorView.swift
+++ weibo/Classes/View/Main/Visitor/VisitorView.swift
@@ -28,6 +28,28 @@ class VisitorView: UIView {
   // MARK: - 懒加载控件
   private lazy var iconView: UIImageView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_smallicon"))
   private lazy var homeIconView: UIImageView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_house"))
+  private lazy var messageLabel: UILabel = {
+    let label = UILabel()
+    label.text = "关注一些人, 回这里看看有什么惊喜"
+    label.textColor = UIColor.darkGray
+    label.font = UIFont.systemFont(ofSize: 14)
+    label.numberOfLines = 0
+    return label
+  }()
+  private lazy var registerButton: UIButton = {
+    let button = UIButton()
+    button.setTitle("注册", for: .normal)
+    button.setTitleColor(UIColor.orange, for: .normal)
+//    button.setBackgroundImage(UIImage(named: "common_button_white_disable"), for: .normal)
+    return button
+  }()
+  private lazy var loginButton = {
+    let button = UIButton()
+    button.setTitle("登录", for: .normal)
+    button.setTitleColor(UIColor.darkGray, for: .normal)
+//    button.setBackgroundImage(UIImage(named: "common_button_white_disable"), for: .normal)
+    return button
+  }()
 }
 
 extension VisitorView {
@@ -36,6 +58,9 @@ extension VisitorView {
     // 1. 添加控件
     addSubview(iconView)
     addSubview(homeIconView)
+    addSubview(messageLabel)
+    addSubview(registerButton)
+    addSubview(loginButton)
     // 2. 设置自动布局
     // 添加约束需要添加到父视图上
     // 建议，子视图最好有一个统一的参照物
@@ -44,14 +69,29 @@ extension VisitorView {
     for v in subviews {
       v.translatesAutoresizingMaskIntoConstraints = false
     }
-//    iconView.translatesAutoresizingMaskIntoConstraints = false
-//    homeIconView.translatesAutoresizingMaskIntoConstraints = false
+    //    iconView.translatesAutoresizingMaskIntoConstraints = false
+    //    homeIconView.translatesAutoresizingMaskIntoConstraints = false
     // 1 > 图标
     addConstraint(NSLayoutConstraint(item: iconView, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1.0, constant: 0));
     addConstraint(NSLayoutConstraint(item: iconView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1.0, constant: 0));
     // 2 > 小房子
     addConstraint(NSLayoutConstraint(item: homeIconView, attribute: .centerX, relatedBy: .equal, toItem: iconView, attribute: .centerX, multiplier: 1.0, constant: 0));
     addConstraint(NSLayoutConstraint(item: homeIconView, attribute: .centerY, relatedBy: .equal, toItem: iconView, attribute: .centerY, multiplier: 1.0, constant: 0));
+    // 3 > 消息文字
+    addConstraint(NSLayoutConstraint(item: messageLabel, attribute: .centerX, relatedBy: .equal, toItem: iconView, attribute: .centerX, multiplier: 1.0, constant: 0))
+    addConstraint(NSLayoutConstraint(item: messageLabel, attribute: .top, relatedBy: .equal, toItem: iconView, attribute: .bottom, multiplier: 1.0, constant: 16))
+    addConstraint(NSLayoutConstraint(item: messageLabel, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 224))
+    addConstraint(NSLayoutConstraint(item: messageLabel, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 36))
+    // 4 > 注册按钮
+    addConstraint(NSLayoutConstraint(item: registerButton, attribute: .left, relatedBy: .equal, toItem: messageLabel, attribute: .left, multiplier: 1.0, constant: 0))
+    addConstraint(NSLayoutConstraint(item: registerButton, attribute: .top, relatedBy: .equal, toItem: messageLabel, attribute: .bottom, multiplier: 1.0, constant: 16))
+    addConstraint(NSLayoutConstraint(item: registerButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 100))
+    addConstraint(NSLayoutConstraint(item: registerButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 36))
+    // 5 > 登录按钮
+    addConstraint(NSLayoutConstraint(item: loginButton, attribute: .right, relatedBy: .equal, toItem: messageLabel, attribute: .right, multiplier: 1.0, constant: 0))
+    addConstraint(NSLayoutConstraint(item: loginButton, attribute: .top, relatedBy: .equal, toItem: messageLabel, attribute: .bottom, multiplier: 1.0, constant: 16))
+    addConstraint(NSLayoutConstraint(item: loginButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 100))
+    addConstraint(NSLayoutConstraint(item: loginButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 36))
   }
   
 }
