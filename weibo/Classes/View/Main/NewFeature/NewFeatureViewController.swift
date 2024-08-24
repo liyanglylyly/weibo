a1ce5dc9987a58b2969e170d18def66e9d74d704
liyang <liyang@huocheetc.com>
2024-08-24 11:34:22 +0800

    UICollectionView 设置布局新特性

--- weibo/Classes/View/Main/NewFeature/NewFeatureViewController.swift
+++ weibo/Classes/View/Main/NewFeature/NewFeatureViewController.swift
@@ -7,15 +7,22 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
+private let NewFeatureViewCellId = "NewFeatureViewCellId"
+private let NewFeatureImageCount = 4
 class NewFeatureViewController: UICollectionViewController {
   
   // 懒加载属性, 必须要在控制器实例化之后才会被创建
   
   init() {
     let layout = UICollectionViewFlowLayout()
+    layout.itemSize = UIScreen.main.bounds.size
+    layout.minimumInteritemSpacing = 0
+    layout.minimumLineSpacing = 0
+    layout.scrollDirection = .horizontal
     super.init(collectionViewLayout: layout)
+    collectionView?.isPagingEnabled = true
+    collectionView.bounces = false
+    collectionView.showsHorizontalScrollIndicator = false
   }
   
   required init?(coder: NSCoder) {
@@ -24,21 +31,24 @@ class NewFeatureViewController: UICollectionViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    // 注册可重用cell
+    self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: NewFeatureViewCellId)
 
   }
 
   override func numberOfSections(in collectionView: UICollectionView) -> Int {
-    return 0
+    return 1
   }
 
 
+  // 每个分组中格子的数量
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 0
+    return NewFeatureImageCount
   }
 
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NewFeatureViewCellId, for: indexPath)
+    cell.backgroundColor = indexPath.item % 2 == 0 ? UIColor.red : UIColor.yellow
         
     return cell
   }
