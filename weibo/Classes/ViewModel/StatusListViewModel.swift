e00dabb48773407e2d9253181026b39971fb276b
liyang <liyang@huocheetc.com>
2024-09-01 16:58:53 +0800

    缓存单张图片

--- weibo/Classes/ViewModel/StatusListViewModel.swift
+++ weibo/Classes/ViewModel/StatusListViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SDWebImage
 
 class StatusListViewModel {
     
@@ -34,6 +35,47 @@ class StatusListViewModel {
       self.statusList = dataList + self.statusList
       // 4. 完成回调
       finished(true)
+      
+      // 5. 缓存单张图片
+      self.cacheSingleImage(dataList: self.statusList)
+    }
+  }
+  
+  /// 缓存单张图片
+  private func cacheSingleImage(dataList: [StatusViewModel]) {
+    // 1. 创建调度组
+    let group = DispatchGroup()
+    let queue = DispatchQueue(label: "cache_images")
+    var dataLength = 0
+    for vm in dataList {
+      // 判断图片数量是否是单张图片
+      if vm.thumbnailUrls?.count != 1 {
+        continue
+      }
+      // 获取url
+      let url = vm.thumbnailUrls![0]
+      print("开始缓存 \(url)")
+      // SDWebImage 下载图像
+      group.enter()
+      queue.async {
+        SDWebImageManager.shared.loadImage(
+          with: url,
+          options: [SDWebImageOptions.refreshCached, SDWebImageOptions.retryFailed],
+          context: nil,
+          progress: nil
+        ) { image, _, _, _, _, _ in
+          // 单张图片下载完成, 计算长度
+          if image != nil {
+            dataLength += NSData(data: image!.pngData()!).length
+          }
+          //
+          group.leave()
+        }
+      }
+    }
+    // 监听调度组完成
+    group.notify(queue: queue) {
+      print("缓存成功 \(dataLength / 1024) K")
     }
   }
 }
